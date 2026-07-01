eb88d321f1c70c7198519c1e33eb97409e43cb07
max rovkin <rovkinmax@gmail.com>
2026-07-01 20:50:39 +0500

    Bump version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     alias(libs.plugins.androidx.baselineprofile)
 }
 
-val currentVersion = "1.1.0"
+val currentVersion = "1.2.0"
 
 /**
  * Reads CLIENT_SECRET from local.properties or system environment variable
