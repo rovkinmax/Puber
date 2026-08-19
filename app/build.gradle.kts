565c190e7b36706438d275bf4fc347e083fee5b0
max rovkin <rovkinmax@gmail.com>
2026-08-19 17:58:13 +0500

    Bump version to 1.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     alias(libs.plugins.androidx.baselineprofile)
 }
 
-val currentVersion = "1.7.0"
+val currentVersion = "1.8.0"
 
 /**
  * Reads CLIENT_SECRET from local.properties or system environment variable
